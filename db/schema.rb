aa2da6e9b4eeb3e99f48f49307857e9af9fc7dca
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-03 16:56:32 +0900

    Add nickname column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803073729) do
+ActiveRecord::Schema.define(version: 20160803075601) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160803073729) do
     t.text     "member",                 limit: 65535
     t.text     "profile",                limit: 65535
     t.text     "works",                  limit: 65535
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
