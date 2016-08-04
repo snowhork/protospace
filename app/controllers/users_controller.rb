ef9b11b366ccdfba7158947f3dd4a7416a9ebce7
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-04 22:55:45 +0900

    Add edit, update action to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    User.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params[:email, :password, :member, :profile, :works]
+  end
 end
