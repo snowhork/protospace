a5fef4ec518eaa84e065123fd3ed7d3db05091db
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-10 11:24:58 +0900

    Set association between image and prototype

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   mount_uploader :substance, SubstanceUploader
+
+  belogs_to :prototype
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,3 @@
 class Prototype < ActiveRecord::Base
+  has_many :images
 end
