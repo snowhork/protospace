a817beff1c0df697dfdab9251001bb8d8f760d7c
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-10 14:47:51 +0900

    Add newline

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,6 +23,11 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:substance]).merge(user_id: current_user.id)
+    params.require(:prototype)
+          .permit(:title,
+                  :catch_copy,
+                  :concept,
+                  images_attributes: [:substance])
+          .merge(user_id: current_user.id)
   end
 end
