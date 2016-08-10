a5188bb8bdc900575a0b59636a4bdbf54b317d97
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-10 13:03:07 +0900

    Create image upload function

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,11 +4,25 @@ class PrototypesController < ApplicationController
 
   def new
     @prototype = Prototype.new
+    3.times { @prototype.images.build }
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+
+    if @prototype.save
+      redirect_to root_path, success: 'Upload your prototype successfully'
+    else
+      render action: 'new'
+    end
   end
 
   def show
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:substance]).merge(user_id: current_user.id)
   end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,5 @@
 class Prototype < ActiveRecord::Base
   has_many :images
+
+  accepts_nested_attributes_for :images
 end
