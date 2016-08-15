053837b4cd76a924ddb0c274fbf424df60820bdc
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-15 21:55:13 +0900

    Set association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,9 +1,19 @@
 class Prototype < ActiveRecord::Base
   has_many :images
 
+  belongs_to :user
+
   accepts_nested_attributes_for :images
 
   validate :prototype_must_have_main_image_to_upload_some_images, on: :create
+
+  def main_image
+    images.each do |image|
+      return image if image.main_flag
+    end
+  end
+
+  private
 
   def prototype_must_have_main_image_to_upload_some_images
     if images.present?
