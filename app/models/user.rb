2141e087bd4f9dc0eff44a027690f288e54af0f8
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-17 21:01:09 +0900

    Modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :prototypes
+  has_many :prototypes, dependent: :destroy
+
+  accepts_nested_attributes_for :prototypes
 end
