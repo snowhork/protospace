add8e93de8341beef8898da27de8d8512a5bcee9
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-20 03:13:43 +0900

    Create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819143159) do
+ActiveRecord::Schema.define(version: 20160819181213) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",         limit: 65535
+    t.integer  "prototype_id", limit: 4
+    t.integer  "user_id",      limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "images", force: :cascade do |t|
     t.string   "substance",    limit: 255
