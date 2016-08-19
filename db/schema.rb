6fca9c95766964e7e9added2dba5c7396b2aeaca
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-20 03:48:08 +0900

    Set default value for likes_count, comments_count

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819181213) do
+ActiveRecord::Schema.define(version: 20160819184612) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20160819181213) do
     t.text     "catch_copy",     limit: 65535
     t.text     "concept",        limit: 65535
     t.integer  "user_id",        limit: 4
-    t.integer  "comments_count", limit: 4
-    t.integer  "likes_count",    limit: 4
+    t.integer  "comments_count", limit: 4,     default: 0
+    t.integer  "likes_count",    limit: 4,     default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title",          limit: 255
