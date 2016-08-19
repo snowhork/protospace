ee6511fd15bdff18a19f81e68034df0c1af59646
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-20 03:45:01 +0900

    Create create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    comment.save
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+          .merge(prototype_id: params[:prototype_id],
+                 user_id: current_user.id)
+  end
 end
