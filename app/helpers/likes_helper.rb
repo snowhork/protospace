226d93c71c336c64499a4663e6f6d0c75c9e6cae
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-20 14:49:40 +0900

    Use likes helper

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -2,4 +2,22 @@ module LikesHelper
   def user_likes_prototype?(user, prototype)
     Like.find_by(user_id: user.id, prototype_id: prototype.id).present?
   end
+
+  def like_form(like, prototype)
+    if user_signed_in?
+      if user_likes_prototype?(current_user, prototype)
+        form_tag prototype_like_path(prototype.id, like.id), method: :delete, remote: true
+      else
+        form_tag prototype_likes_path(prototype.id), method: :post, remote: true
+      end
+    end
+  end
+
+  def like_icon(prototype)
+    if user_signed_in? && user_likes_prototype?(current_user, prototype)
+      "icon_heart_red.svg"
+    else
+      "icon_heart.svg"
+    end
+  end
 end
