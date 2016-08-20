f28fc79f6be21263920484e5d1c8b91feb09e587
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-20 12:30:34 +0900

    Modify likes_controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new(like_params)
-    @like.save
+    if user_signed_in?
+      @like = Like.new(like_params)
+      unless @like.save
+        flash.now[:danger] = comment.errors.full_messages[0]
+      end
+    end
     set_prototype
   end
 
   def destroy
-    @like = Like.find(params[:id])
-    @like.destroy
+    like = Like.find(params[:id])
+    if is_instance_current_users?(like)
+      unless like.destroy
+        flash.now[:danger] = comment.errors.full_messages[0]
+      end
+    end
     set_prototype
   end
 
