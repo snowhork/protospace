2bb612c743b557e838871103dfb7f7acfad45ade
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-24 15:33:49 +0900

    Change comment_nickname to user_nickname

--- app/helpers/comments_helper.rb
+++
@@ -1,9 +0,0 @@
-module CommentsHelper
-  def comment_nickname(user)
-    if user.present?
-      user.nickname
-    else
-      "no name"
-    end
-  end
-end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,13 @@ module UsersHelper
   def is_instance_current_users?(instance)
     user_signed_in? && instance.user.id == current_user.id
   end
+
+  def user_nickname(user)
+    if user.present?
+      user.nickname
+    else
+      "no name"
+    end
+  end
+
 end
