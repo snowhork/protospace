4d8bf66d270a722f70a4a5b334f1dcda0fdcbd07
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-29 00:25:07 +0900

    Modify index action, order created_at DESC

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,7 +5,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.all.includes(:user).page(params[:page])
+    @prototypes = Prototype.includes(:user).page(params[:page]).order('created_at DESC')
   end
 
   def new
