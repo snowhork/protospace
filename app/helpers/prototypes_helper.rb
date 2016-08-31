66c60d4ec2461f9ff9ce02a10adfe859f4be5579
まるまる <kyoh@marumaru-no-MacBook-Air.local>
2016-08-31 18:19:20 +0900

    Delete comment out

--- app/helpers/prototypes_helper.rb
+++ app/helpers/prototypes_helper.rb
@@ -1,6 +1,5 @@
 module PrototypesHelper
   def switch_proto_class(controller)
-    # controller == params[:controller] ? {class: 'active', role: "presentation"} : {role: "presentation"}
     'active' if controller == params[:controller]
   end
 end
