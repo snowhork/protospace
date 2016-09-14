1179f97a895e4f26f674e28a0d778b4c3d3ed461
まるまる <romio_cinderella@yahoo.co.jp>
2016-09-14 13:45:40 +0900

    Test without a email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,19 @@ RSpec.describe User, :type => :model do
 
     describe 'without valid attributes' do
       it 'is invalid without a nickname' do
-        user = build(:user, nickname: '')
+        user = build(:user, nickname: nil)
         user.valid?
         expect(user.errors[:nickname]).to include("can't be blank")
       end
+
+      it 'is invalid without a email' do
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+
+
     end
-    
+
   end
 end
