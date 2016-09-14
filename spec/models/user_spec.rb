4d5d173b7699f893264252f8900d5236cd1c79bd
まるまる <romio_cinderella@yahoo.co.jp>
2016-09-14 13:43:29 +0900

    Setting validation nickname presence: true

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe '#create' do
-    it "is valid with a nickname, email, password, password_confirmation" do
-      user = build(:user)
-      expect(user).to be_valid
+    describe 'with valid attribtues' do
+      it "is valid with a nickname, email, password, password_confirmation" do
+        user = build(:user)
+        expect(user).to be_valid
+      end
     end
+
+    describe 'without valid attributes' do
+      it 'is invalid without a nickname' do
+        user = build(:user, nickname: '')
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+    end
+    
   end
 end
